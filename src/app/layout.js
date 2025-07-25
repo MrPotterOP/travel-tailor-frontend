bbbc567d6fb31c3e84058d3236430e57ab7d714d
MrPotterOP <shubham.ubarhande1044@gmail.com>
2025-07-25 17:57:26 +0530

    gtag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Caveat, Lexend, Open_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 import WhatsAppButton from "./components/UI/Button/Whatsapp";
@@ -18,6 +19,8 @@ const fontPrime = Open_Sans({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700"],
 })
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTAG_ID;
 
 
 import Navbar from "./components/Navbar/Navbar";
@@ -50,7 +53,36 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+      <head>
+        {/* Google Tag Manager Script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+          }}
+        />
+      </head>
+
       <body className={`${fontPrime.variable} ${fontAlt.variable}`} suppressHydrationWarning>
+
+        {/* Google Tag Manager NoScript Fallback */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         <Navbar />
         {children}
         <Footer />
